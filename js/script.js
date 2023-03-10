c739aedca91dd09d741aedbe69814dfc977c947e
Ivan12341241 <vanooo123@i.ua>
2023-03-10 23:05:57 +0300

    hmm, nice

--- js/script.js
+++ js/script.js
@@ -15,7 +15,7 @@ function init_slider(slider) {
     next.addEventListener('click', function() {
         slide[i].classList.remove('block');
 
-        i = (i + 1) % slide.length; // (*1)
+        i = (i + 1) % slide.length;
 
         slide[i].classList.add('block');
     });
@@ -26,7 +26,6 @@ function init_slider(slider) {
 function r(f) { /in/.test(document.readyState) ? setTimeout('r(' + f + ')', 9) : f() }
 r(function() {
     if (!document.getElementsByClassName) {
-        // IE8 support
         var getElementsByClassName = function(node, classname) {
             var a = [];
             var re = new RegExp('(^| )' + classname + '( |$)');
@@ -42,27 +41,23 @@ r(function() {
 
     var nb_videos = videos.length;
     for (var i = 0; i < nb_videos; i++) {
-        // Based on the YouTube ID, we can easily find the thumbnail image
         videos[i].style.backgroundImage = 'url(http://i.ytimg.com/vi/' + videos[i].id + '/sddefault.jpg)';
 
-        // Overlay the Play icon to make it look like a video player
         var play = document.createElement("div");
         play.setAttribute("class", "play");
         videos[i].appendChild(play);
 
         videos[i].onclick = function() {
-            // Create an iFrame with autoplay set to true
             var iframe = document.createElement("iframe");
-            var iframe_url = "https://www.youtube.com/embed/" + this.id + "?autoplay=1&autohide=1";
+            var iframe_url = "https://www.youtube-nocookie.com/embed/" + this.id + "?autoplay=1&autohide=1";
             if (this.getAttribute("data-params")) iframe_url += '&' + this.getAttribute("data-params");
             iframe.setAttribute("src", iframe_url);
             iframe.setAttribute("frameborder", '0');
-
-            // The height and width of the iFrame should be the same as parent
+            iframe.setAttribute("allow", "accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share");
+            iframe.setAttribute("allowfullscreen", "allowfullscreen");
             iframe.style.width = this.style.width;
             iframe.style.height = this.style.height;
 
-            // Replace the YouTube thumbnail with YouTube Player
             this.parentNode.replaceChild(iframe, this);
         }
     }
